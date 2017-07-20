39827d4d305629c25d38ff21d0476a6065ea04cc
YingjiaZhang <zhyingjia@163.com>
2017-07-20 18:20:10 +0800

    update header

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -1,6 +1,23 @@
 <template>
   <div class="header">
-    
+    <div class="content-wrapper">
+      <div class="avatar">
+         <img width="64px" height="64px" :src="seller.avatar"/>
+      </div>
+    </div>
+    <div class="bulletin-wrapper">
+      <div class="title">
+        <span class="brand"></span>
+        <span class="name">{{seller.name}}</span>
+      </div>
+      <div class="descriptions">
+        {{seller.description}}/{{seller.deliveryTime}}分钟到达
+      </div>
+      <div v-if="seller.supports" class="supports">
+        <span class="icon"></span>
+        <span calss="text">{{seller.supports[0].description}}</span>
+      </div>
+    </div>
   </div>
 </template>
 
